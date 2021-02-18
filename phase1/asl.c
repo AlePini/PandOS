a7b007d3814ac51400d03e8ef5de6b9c7f8a36b8
Yureeh <fabbri.juri@gmail.com>
2021-02-18 19:02:42 +0100

    asl.h and asl.c are completed

--- phase1/asl.c
+++ phase1/asl.c
@@ -1,5 +1,9 @@
 #include <asl.h>
 
+/**This function works also when the semaphore we're searching for is not present in the ASL.
+*The function will return the last semaphore before the one whose identifier is MAXINT and
+*the checks in the other functions will handle error situations.
+*/
 
 HIDDEN semd_t* findPrevSem(int* semAdd){
     //Scan the ASL until the next semaphore identifier is bigger than semAdd
@@ -11,18 +15,22 @@ HIDDEN semd_t* findPrevSem(int* semAdd){
 }
 
 int insertBlocked(int *semAdd, pcb_t *p){
-    if (p == NULL) return FALSE;        //TODO: serve davvero?
+    //Checks if the PCB pointer passed is not NULL
+    if (p == NULL) return FALSE;
+
     semd_t* prev = findPrevSem(semAdd);
     semd_t* next = prev->s_next;
-    //If there's the semaphore with the correct identifier inserts the process p.
+    //If the semaphore with the correct identifier is found the function inserts the process p.
     if(next->s_semAdd == semAdd){
         p->p_semAdd  = semAdd;
         insertProcQ(&next->s_procQ, p);
     }
     //Otherwise it has to allocate a new semaphore with the right identifier.
     else{
+
         //If the list of the free semaphores is empty returns true.
         if (semdFree_h == NULL) return TRUE;
+
         //Otherwise allocates and initializes one.
         semd_t* toInsert = semdFree_h;
         semdFree_h = semdFree_h -> s_next;
@@ -30,6 +38,7 @@ int insertBlocked(int *semAdd, pcb_t *p){
         toInsert -> s_procQ = mkEmptyProcQ();
         p->p_semAdd  = semAdd;
         insertProcQ(&toInsert->s_procQ, p);
+
         //Then inserts it in the list of the actives semaphores.
         toInsert -> s_next = prev-> s_next;
         prev -> s_next = toInsert;
@@ -47,7 +56,7 @@ pcb_t* removeBlocked(int *semAdd){
 
     //Otherwise removes the head of the queue of the processes.
     pcb_t* removed = removeProcQ(&next->s_procQ);
-    //If by doing so the processes' queue becomes empty, deallocates the semaphore.
+    //If by doing so the process queue becomes empty, deallocates the semaphore.
     if(emptyProcQ(next->s_procQ)){
         //Returns the empty semaphore to the semdFree.
         prev->s_next=next->s_next;
@@ -58,9 +67,17 @@ pcb_t* removeBlocked(int *semAdd){
     return removed;
 }
 
-    //Otherwise removes p from the processes' queue.
+pcb_t* outBlocked(pcb_t *p) {
+    //Checks if the PCB pointer passed is not NULL
+    if (p == NULL) return NULL;
+    semd_t* prev = findPrevSem(p->p_semAdd);
+    semd_t* next = prev->s_next;
+
+    //If the semaphore is not in the ASL returns NULL.
+    if(next->s_semAdd != p->p_semAdd) return NULL;
+    //Otherwise removes p from the process queue.
     pcb_t *toRemove = outProcQ(&(next->s_procQ), p);
-    //If by doing so the processes' queue becomes empty, deallocates the semaphores.
+    //If by doing so the process queue becomes empty, deallocates the semaphores.
     if(emptyProcQ(next->s_procQ)){
         //Returns the empty semaphores to the semdFree.
         prev->s_next=next->s_next;
@@ -74,10 +91,10 @@ pcb_t* removeBlocked(int *semAdd){
 pcb_t* headBlocked(int *semAdd){
     semd_t* prev = findPrevSem(semAdd);
     semd_t* next = prev->s_next;
-    //If the semaphores is not there returns NULL.
+    //If the semaphores is not in the ASL returns NULL.
     if(next->s_semAdd != semAdd)
         return NULL;
-    //Otherwise returns the head of the processes' queue.
+    //Otherwise returns the head of the process queue.
     return headProcQ(next->s_procQ);
 }
 
