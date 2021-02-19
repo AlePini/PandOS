6be8f6da8b3cb0f2a302c91c72c12e76306e56a7
Yureeh <fabbri.juri@gmail.com>
2021-02-19 16:31:22 +0100

    cancelled the trailing spaces

--- h/pcb.h
+++ h/pcb.h
@@ -38,7 +38,7 @@ void insertProcQ(pcb_t **tp, pcb_t *p);
 
 /**
  * @brief Returns the pointer to the head of the tp process queue, without removing it.
- * 
+ *
  * @param tp The pointer to the tail of the process queue.
  * @return The pointer to the head of the process queue, NULL if the queue is empty.
  */
@@ -46,7 +46,7 @@ pcb_t *headProcQ(pcb_t *tp);
 
 /**
  * @brief Removes the oldest element (the head) from the tp queue.
- * 
+ *
  * @param tp The pointer to the queue.
  * @return The pointer to the element removed from the list, NULL if the queue is empty.
  */
@@ -54,7 +54,7 @@ pcb_t *removeProcQ(pcb_t **tp);
 
 /**
  * @brief Removes the PCB pointed by P from the process queue pointed by tp.
- * 
+ *
  * @param tp The pointer to the queue.
  * @param p The pointer to the PCB that has to be removed.
  * @return The pointer to the removed PCB, NULL if the PCB pointed by p is not in the queue.
@@ -81,7 +81,7 @@ HIDDEN pcb_t* trim(pcb_t *p);
 
 /**
  * @brief Inspects if the PCB pointed by p has a child.
- * 
+ *
  * @param p The pointer to the PCB that has to be inspected.
  * @return TRUE if the PCB pointed by p has no children, FALSE otherwise.
  */
@@ -89,7 +89,7 @@ int emptyChild(pcb_t *p);
 
 /**
  * @brief Inserts the PCB pointed by p as a child of the PCB pointed by prnt.
- * 
+ *
  * @param prnt The pointer to the PCB which will become parent of p.
  * @param p The pointer to the PCB which will become child of prnt.
  */
@@ -97,7 +97,7 @@ void insertChild(pcb_t *prnt, pcb_t *p);
 
 /**
  * @brief Removes the first child of the PCB pointed by p.
- * 
+ *
  * @param p The pointer to the PCB whose first child will be removed.
  * @return The pointer to the first child of the PCB, NULL if the PCB doesn't have a child.
  */
@@ -105,7 +105,7 @@ pcb_t *removeChild(pcb_t *p);
 
 /**
  * @brief Removes the PCB pointed by p from the list of his parent's children.
- * 
+ *
  * @param p The pointer to the PCB that will be removed.
  * @return The pointer to the PCB, NULL if the PCB doesn't have a parent.
  */
