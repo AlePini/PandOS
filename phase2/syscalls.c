71dcb6470e756d21a289e32e01af6333d34e9c90
ErGiacuz <leonisio300@gmail.com>
2021-03-22 18:08:27 +0100

    termina processo

--- phase2/syscalls.c
+++ phase2/syscalls.c
@@ -1,6 +1,21 @@
 #include <syscall.h>
 #include <initial.h>
 #include <utils.h>
+
+
+
+
+void Terminate_process(pcb_t* p){
+    if(p == NULL) return;
+    if(!emptyChild(p)) Terminate_process(p -> p_child);
+    if(p -> p_next_sib != NULL) Terminate_process(p -> p_next_sib);
+    outChild(p);
+    outProcQ(getReadyQueue(), p);
+    /* Da fare verifica sul semaforo se è negativo e non device bisogna aggiustarne il valore */
+    outBlocked(p);
+    freePcb(p); 
+}
+
 void passeren(int* semaddr){
     (*semaddr)--;
     if(*semaddr <= 0){
