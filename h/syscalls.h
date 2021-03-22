f612cce2bae764b956952bf69acdc6e6d3a73a2e
Pino <alessandro.pini.filippini@gmail.com>
2021-03-22 18:37:50 +0100

    Implemented SYS 1, 6-8

--- h/syscalls.h
+++ h/syscalls.h
@@ -1,3 +1,16 @@
+#ifndef SYSCALL_H
+#define SYSCALL_H
 
+void sysHandler();
+
+/* SYSCALLS */
+void createProcess();
+void terminateProcess();
 void passeren(int*);
 void verhogen(int*);
+void waitIO(int, int, int);
+void getCPUTime();
+void waitForClock();
+void getSupportData();
+
+#endif
--- phase2/syscalls.c
+++ phase2/syscalls.c
@@ -4,20 +4,9 @@
 #include <exception.h>
 
 
-void Terminate_process(pcb_t* p){
-    if(p == NULL) return;
-    if(!emptyChild(p)) Terminate_process(p -> p_child);
-    if(p -> p_next_sib != NULL) Terminate_process(p -> p_next_sib);
-    outChild(p);
-    outProcQ(getReadyQueue(), p);
-    /* Da fare verifica sul semaforo se è negativo e non device bisogna aggiustarne il valore */
-    outBlocked(p);
-    freePcb(p); 
-
-
 void sysHandler(){
     //Leggo l'id della syscall
-    unsigned sysNumber = current->p_s.reg_a0;
+    unsigned sysNumber = currentProcess->p_s.reg_a0;
     if(sysNumber >=1 && sysNumber<=8){
         //Se è fra 1 e 8 devo essere in kernel mode altrimenti eccezione
         currentProcess->p_s = EXCTYPE;
@@ -27,10 +16,10 @@ void sysHandler(){
         }
         else{
             //Se va tutto bene controllo che syscall sia
-            unsigned arg1 = current->p_s.reg_a1;
-            unsigned arg2 = current->p_s.reg_a2;
-            unsigned arg3 = current->p_s.reg_a3;
-            currentProcess->p_s.pc_epc = current->p_s.pc_epc + 4;
+            unsigned arg1 = currentProcess->p_s.reg_a1;
+            unsigned arg2 = currentProcess->p_s.reg_a2;
+            unsigned arg3 = currentProcess->p_s.reg_a3;
+            currentProcess->p_s.pc_epc += 4;
             switch(sysNumber){
                 case CREATEPROCESS:
                     createProcess((state_t*) arg1, (support_t*) arg2);
@@ -68,6 +57,32 @@ void sysHandler(){
     }
 }
 
+void createProcess(){
+
+}
+
+void termiateProcess(){
+    terminateRecursive(currentProcess);
+    // TODO check for exception handler
+    scheduler();
+    return;
+}
+
+void terminateRecursive(pcb_t* p){
+    if(p == NULL) return;
+    if(!emptyChild(p))
+        terminateRecursive(p -> p_child);
+    if(p -> p_next_sib != NULL)
+        terminateRecursive(p -> p_next_sib);
+    
+    outChild(p);
+    outProcQ(getReadyQueue(), p);
+    /* Da fare verifica sul semaforo se è negativo e non device bisogna aggiustarne il valore */
+    outBlocked(p);
+    freePcb(p);
+    return;
+}
+
 void passeren(int* semaddr){
     (*semaddr)--;
     if(*semaddr <= 0){
@@ -84,5 +99,26 @@ void verhogen(int* semaddr){
         pcb_t* tmp = removeBlocked(semaddr);
         insertProcQ(getReadyQueue(),tmp);
     }
+    return;
+}
+
+void waitIO(){
+    
+}
+
+// TODO + CPU time used during the current quantum/time slice;
+void getCPUTime(){
+    unsigned time = currentProcess->p_time;
+    currentProcess->p_s.reg_v0 = time;
+    return;
+}
+
+void waitForClock(){
+    passeren(&semIntTimer);
+    return;
+}
+
+void getSupportData(){
+    currentProccess->p_s.reg_v0 = currentProcess->p_supportStruct;
     return;
 }
