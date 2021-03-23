aec87da79c0613d38239cc29521d73c8f36c4142
Pino <alessandro.pini.filippini@gmail.com>
2021-03-23 12:21:57 +0100

    Implemented memcpy, 'cause ld is bad

--- h/exceptions.h
+++ h/exceptions.h
@@ -1,6 +1,12 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
 #include <initial.h>
 #include <scheduler.h>
 #include <umps3/umps/libumps.h>
 #include <syscalls.h>
+#include <memory.h>
 
 extern pcb_t* currentProcess;
+
+#endif
