d8edf1fb8dae5aa1cbb065d6aed01ff2f86d95e3
Pino <alessandro.pini.filippini@gmail.com>
2021-09-04 16:28:56 +0200

    Removed breakpoints

--- phase2/memory.c
+++ phase2/memory.c
@@ -1,14 +1,9 @@
 #include <memory.h>
 
-void mem_break(){
-    return;
-}
-
 void *memcpy(void *dest, const void *src, size_t n){
     char *d = dest;
     const char *s = src;
     while (n--)
        *d++ = *s++;
-    mem_break();
     return dest;  
 }
